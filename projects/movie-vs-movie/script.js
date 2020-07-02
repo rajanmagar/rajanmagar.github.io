8e97501b64f849b1998cac035d2e13a22ec8a40d
rajanmagar <rajan@moengage.com>
2020-07-03 04:46:57 +0530

    added movie template :fire:

--- projects/movie-vs-movie/script.js
+++ projects/movie-vs-movie/script.js
@@ -61,5 +61,44 @@ document.addEventListener('click', (event) => {
 
 const onMovieSelect = async (movie) => {
   const response = await (await fetch(requestMovie(movie.imdbID))).json();
-  console.log(response);
+  document.querySelector('#summary').innerHTML = movieTemplate(response);
+};
+
+const movieTemplate = (detail) => {
+  return `
+    <article class="media">
+      <figure class="media-left">
+        <p class="image">
+          <img src="${detail.Poster}" />
+        </p>
+      </figure>
+      <div class="media-content">
+        <div class="content">
+          <h1>${detail.Title}</h1>
+          <h4>${detail.Genre}</h4>
+          <p>${detail.Plot}</p>
+        </div>
+      </div>
+    </article>
+    <article class="notification">
+      <p class="title">${detail.Awards}</p>
+      <p class="subtitle">Awards</p>
+    </article>
+    <article class="notification">
+      <p class="title">${detail.BoxOffice}</p>
+      <p class="subtitle">Box office</p>
+    </article>
+    <article class="notification">
+      <p class="title">${detail.Metascore}</p>
+      <p class="subtitle">Metascore</p>
+    </article>
+    <article class="notification">
+      <p class="title">${detail.imdbRating}</p>
+      <p class="subtitle">IMDB Rating</p>
+    </article>
+    <article class="notification">
+      <p class="title">${detail.imdbVotes}</p>
+      <p class="subtitle">IMDB Votes</p>
+    </article>
+  `;
 };
